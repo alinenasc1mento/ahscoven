3b3d873407c3635c6f6d8eb2bf31d1782e0daafd
Aline Nascimento <139106587+alinenasc1mento@users.noreply.github.com>
2023-07-23 19:45:08 -0300

    Update jquery.nivo.slider.js

--- nivo-slider/jquery.nivo.slider.js
+++ nivo-slider/jquery.nivo.slider.js
@@ -1,12 +1,4 @@
-/*
- * jQuery Nivo Slider v3.2
- * http://nivo.dev7studios.com
- *
- * Copyright 2012, Dev7studios
- * Free to use and abuse under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- */
-
+ 
 (function($) {
     var NivoSlider = function(element, options){
         // Defaults are below
